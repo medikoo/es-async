7c9222828461ca2f7136ed38696a4f5c841509d2
Mariusz Nowak <medyk@medikoo.com>
2018-08-09 12:27:41 +0200

    style: prettier

--- test/index.js
+++ test/index.js
@@ -6,12 +6,13 @@ const test      = require("tape")
 test("Transpiler", t => {
 	t.test("Async code is transpiled", t =>
 		// Just sanity check, as all transpilation tests are handled in nodent project.
-		eval(transpile("(async foo => (foo + await new Promise(resolve => resolve(4))))"))(
-			3
-		).then(result => {
-			t.equal(result, 7);
-			t.end();
-		}));
+		eval(transpile("(async foo => (foo + await new Promise(resolve => resolve(4))))"))(3).then(
+			result => {
+				t.equal(result, 7);
+				t.end();
+			}
+		)
+	);
 
 	t.test("Non-async code is passed as is", t => {
 		const codeSample = "(foo => new Promise(resolve => resolve(4)))";
